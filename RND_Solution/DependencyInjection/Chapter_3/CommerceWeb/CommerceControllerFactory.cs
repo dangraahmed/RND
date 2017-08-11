bcdb43ec18099d03b3a108daaea80467aaeb5d06
Ahmed Amin Dangra <dangra.ahmed@gmail.com>
2017-08-11 14:42:36 +0530

    DependencyInjection Chapter #3 added

---
+++ RND_Solution/DependencyInjection/Chapter_3/CommerceWeb/CommerceControllerFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CommerceDomain;
+using CommerceWeb.Controllers;
+
+namespace CommerceWeb
+{
+    public class CommerceControllerFactory : DefaultControllerFactory
+    {
+        private readonly Dictionary<string, Func<RequestContext, IController>> controllerMap;
+
+        public CommerceControllerFactory(ProductRepository repository)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException("repository");
+            }
+
+            this.controllerMap = new Dictionary<string, Func<RequestContext, IController>>();
+            this.controllerMap["Home"] = ctx => new HomeController(repository);
+        }
+
+        public override IController CreateController(RequestContext requestContext, string controllerName)
+        {
+            return this.controllerMap[controllerName](requestContext);
+        }
+
+        public override void ReleaseController(IController controller)
+        {
+        }
+    }
+}
