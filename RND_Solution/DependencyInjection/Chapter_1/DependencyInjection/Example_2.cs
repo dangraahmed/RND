4360acd401cd13143f34fd9ca840dc766ddd176a
Ahmed Amin Dangra <dangra.ahmed@gmail.com>
2017-08-11 12:28:46 +0530

    DependencyInjection Chapter #1 added

---
+++ RND_Solution/DependencyInjection/Chapter_1/DependencyInjection/Example_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Chapter_1
+{
+    public class Example_2
+    {
+        public static void MainFunction()
+        {
+            var typeName = ConfigurationManager.AppSettings["messageWriter"];
+            var type = Type.GetType(typeName, false);
+
+            IMessageWriter writer = (IMessageWriter) Activator.CreateInstance(type);
+
+            var salutation = new Salutation(writer);
+            salutation.Exclaim();
+        }
+    }
+}
