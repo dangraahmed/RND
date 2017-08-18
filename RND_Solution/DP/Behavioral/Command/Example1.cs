72d4992ff99052dde04fce151990945623671fc8
Ahmed Amin Dangra <dangra.ahmed@gmail.com>
2017-08-18 18:44:03 +0530

    Factory Design Pattern example 2 added

--- RND_Solution/DP/Behavioral/Command/Example1.cs
+++ RND_Solution/DP/Behavioral/Command/Example1.cs
@@ -68,7 +68,7 @@ namespace DP.Behavioral.Command
 
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main1(string[] args)
         {
             Light lamp = new Light();
             ICommand switchUp = new FlipUpCommand(lamp);
@@ -104,3 +104,15 @@ namespace DP.Behavioral.Command
         }
     }
 }
+
+/*
+The Command Pattern falls under the category of Behavioural Design Patterns.If you have quite amount of experience in C# particularly WPF, 
+you must have used DelegateCommand or Routed Command or RelayCommands. This internally uses the Command Pattern only. 
+The Command Pattern can be used in any of the projects and we will quicky understand what is it and how to use it in our project.
+
+The Command Pattern encapsulates a request as an object and gives it a known public interface. 
+A request or action is mapped and stored as an object. The Invoker will be ultimately responsible for processing the request.
+This clearly decouples the request from the invoker.This is more suited for scenarios where we implement Redo, Copy, Paste and Undo operations where the action is stored as an object. 
+We generally use Menu or Shortcut key gestures for any of the preceding actions to be executed.
+http://www.dotnettricks.com/learn/designpatterns/command-design-pattern-dotnet
+*/
---
+++ RND_Solution/DP/Creational/Factory/Example_2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.Creational.Factory
+{
+    /// <summary>
+    /// The 'Product' interface
+    /// </summary>
+    public interface IFactory
+    {
+        void Drive(int miles);
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class Scooter : IFactory
+    {
+        public void Drive(int miles)
+        {
+            Console.WriteLine("Drive the Scooter : " + miles.ToString() + "km");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class Bike : IFactory
+    {
+        public void Drive(int miles)
+        {
+            Console.WriteLine("Drive the Bike : " + miles.ToString() + "km");
+        }
+    }
+
+    /// <summary>
+    /// The Creator Abstract Class
+    /// </summary>
+    public abstract class VehicleFactory
+    {
+        public abstract IFactory GetVehicle(string Vehicle);
+
+    }
+
+    public class ConcreteVehicleFactory : VehicleFactory
+    {
+        public override IFactory GetVehicle(string Vehicle)
+        {
+            switch (Vehicle)
+            {
+                case "Scooter":
+                    return new Scooter();
+                case "Bike":
+                    return new Bike();
+                default:
+                    throw new ApplicationException($"Vehicle '{Vehicle}' cannot be created");
+            }
+        }
+    }
+
+    class Example_2
+    {
+        public static void Main(string[] args)
+        {
+            VehicleFactory factory = new ConcreteVehicleFactory();
+            IFactory scooter = factory.GetVehicle("Scooter");
+            scooter.Drive(10);
+
+            IFactory bike = factory.GetVehicle("Bike");
+            bike.Drive(100);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+/*
+ * In Factory pattern, we create object without exposing the creation logic. 
+ * In this pattern, an interface is used for creating an object, but let subclass decide which class to instantiate. 
+ * The creation of object is done when it is required. The Factory method allows a class later instantiation to subclasses.
+ * http://www.dotnettricks.com/learn/designpatterns/factory-method-design-pattern-dotnet
+ */
+
